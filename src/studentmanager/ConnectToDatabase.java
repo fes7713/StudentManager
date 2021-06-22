060c003f216263aabfd22dc3cf569d3fd08685d0
Keita Nakashima <username@example.comtun05036@temple.edu>
2021-06-22 12:05:53 +0900

    MySQL Connection

--- src/studentmanager/ConnectToDatabase.java
+++ src/studentmanager/ConnectToDatabase.java
@@ -20,3 +20,14 @@ public class ConnectToDatabase {
             }
         }
 }
+//
+//If anyone having trouble connecting mysql, try this
+//1) Make sure MYSQLURL is correct  
+//jdbc:mysql://localhost:3306/Lab02
+//jdbc:mysql://localhost:port number/Schema name (Its called database name inside -> it confused me for 15 mins)
+//2) Check port number if you have changed it before.
+//3) Make sure you put jar file class path to ur project.
+//Go to ur project setting and add mysql connector .jar
+//Not module path, class path
+//4) Check your password
+//5) You may find error in thr first line import java.sql.*; but its nothing.
